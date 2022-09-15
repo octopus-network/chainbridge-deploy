54daf7444a394e61ef5d525894c59e522aeb2ce4
livelybug <livelybug@gmail.com>
2022-09-15 10:30:22 +0800

    Set ERC token decimal dynamically by "-d"

--- cb-sol-cli/cmd/erc20.js
+++ cb-sol-cli/cmd/erc20.js
@@ -59,7 +59,7 @@ const depositCmd = new Command("deposit")
         // Instances
         const bridgeInstance = new ethers.Contract(args.bridge, constants.ContractABIs.Bridge.abi, args.wallet);
         const data = '0x' +
-            ethers.utils.hexZeroPad(ethers.utils.bigNumberify(expandDecimals(args.amount, 6)).toHexString(), 32).substr(2) +    // Deposit Amount        (32 bytes)
+            ethers.utils.hexZeroPad(ethers.utils.bigNumberify(expandDecimals(args.amount, args.decimals)).toHexString(), 32).substr(2) +    // Deposit Amount        (32 bytes)
             ethers.utils.hexZeroPad(ethers.utils.hexlify((args.recipient.length - 2)/2), 32).substr(2) +    // len(recipientAddress) (32 bytes)
             args.recipient.substr(2);                    // recipientAddress      (?? bytes)
 
